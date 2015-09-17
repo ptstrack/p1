6c18cc9d0e3fd08cf549fa30efa03aa935039a21
Peter Strack <ptstrack@msn.com>
2015-09-17 11:54:52 -0400

    Updated index; added profile pic.

--- index.php
+++ index.php
@@ -1,1 +1,103 @@
-<h1>Placeholder for P1</h1>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Peter Strack | DWA CSCI E-15 Portfolio</title>
+		<meta charset="utf-8">
+		<style type="text/css">
+			body { 
+				margin-left: 10%; 
+				margin-right: 10%; 
+			}
+			h1,h2,h3,h4,h5,h6 { 
+				font-family: Calibri,Verdana,Arial,sans-serif; 
+				border-top: medium solid blue; 
+				padding-top: 0.5em;
+				padding-left: 0.25em;
+			}
+			h1 {
+				text-align:center;
+			}
+			p  {
+				font-family: Calibri,Verdana,Arial,sans-serif; 
+				padding-left: 1.0em;
+				padding-right: 1.0em;
+			}
+			.container {
+				width:50%;
+				min-width:600px;
+				margin-bottom:50px;
+			}
+			img {
+				display:block;
+				margin:auto;
+				width:10%;
+			} 
+		</style>
+	</head>
+	
+	<body>
+		<div class-"container">
+			<h1>
+				Peter Strack
+				<br>
+				Dynamic Web Applications - Project Portfolio
+			</h1>
+			
+			<img alt="Peter Strack" src="Peter1.jpeg" />
+				
+			<h3>Project 2 Title</h3>
+			<ul>
+				<li><a href="http://p2.strack-works.com">View Live Site</a></li>
+				<li><a href="http://www.github.com/ptstrack/p2">Github files</a></li>
+			</ul>
+				
+			<h3>Project 3 Title</h3>
+			<ul>
+				<li><a href="http://p3.strack-works.com">View Live Site</a></li>
+				<li><a href="http://www.github.com/ptstrack/p3">Github files</a></li>
+			</ul>
+			<h3>Project 4 Title</h3>
+			<ul>
+				<li><a href="http://p4.strack-works.com">View Live Site</a></li>
+				<li><a href="http://www.github.com/ptstrack/p4">Github files</a></li>
+			</ul>
+			
+			<h2>Who I am and what I do</h2>
+			<p>
+				Hello - I work in business development and solutions design for a small non-profit healthcare 
+				research and consulting company called the <a href="http://www.altarum.org">Altarum Institute
+				</a>that focuses on improving human health. Recently, I've taken a more active role in improving 
+				our online marketing presence, which means I need to learn more about developing dynamic websites
+				and PHP, while also refreshing my HTML, CSS, and Javascript skills.
+				<br>
+				<br>
+				For fun, I love spending time with my family and friends. I enjoy working out (especially CrossFit), 
+				boating, snowboarding, and exploring new restaurants and events in the Washington, D.C. metro area."
+			</p>
+			<h2>Why I'm taking this course</h2>
+			<p>
+				I'm taking this course for several reasons. First, I've always wanted to know the 
+				underlying technology behind how web applications work and how they're designed to 
+				function. Second, I seem to encounter daily technology challenges with my own company's 
+				software and I'm looking to help bridge the gap between the business end-user - <em>me</em> -  
+				and our IT department. Lastly, but perhaps most importantly, I find learning new concepts
+				and applications to be extremely fun and rewarding. While the grades are important, I really
+				just want to learn how this stuff works so I can have the requisite skills to play around 
+				with it, try new features, and have fun!
+			</p>
+			<h2>Prior experience with programming</h2>
+			<p>
+				My previous experience with programming is a bit limited.  I took CSCI e12 in 2013 which
+				was a great introduction into html, css, and js.  Prior to that, I last took some Pascal in
+				high school and class in C++ about 20 years ago.
+			</p>
+			<h2>Which platform am I using?</h2>
+			<p>
+				I'm a diehard Apple fan - so even though I use a Dell for work, I use a Mac for all
+				programming activities and coursework.
+			</p>
+			<br>
+		</div>
+	</body>
+</html>
+			
